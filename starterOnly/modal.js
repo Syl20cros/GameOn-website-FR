98e1b54a1735fa1f33f6447a6913db8ead8c283a
CROS <syl20cros@gmail.com>
2021-05-19 16:04:37 +0200

    feat (modal) : Add validation and error message if necessary on form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -45,13 +45,25 @@ var first = document.getElementById('first');
 var last = document.getElementById('last');
 var email = document.getElementById('email');
 var birthdate = document.getElementById('birthdate');
+var quantity = document.getElementById('quantity');
+var checkTerms = document.getElementById('checkbox1');
+const city1 = document.getElementById('location1');
+const city2 = document.getElementById('location2');
+const city3 = document.getElementById('location3');
+const city4 = document.getElementById('location4');
+const city5 = document.getElementById('location5');
+const city6 = document.getElementById('location6');
 
 var first_missing = document.getElementById('first_missing');
 var last_missing = document.getElementById('last_missing');
 var errorEmail = document.getElementById('errorEmail');
 var errorBirthdate = document.getElementById('errorBirthdate');
+var errorParticipation = document.getElementById('errorParticipation');
+var errorCity = document.getElementById('errorCity');
+var errorCondition = document.getElementById('errorCondition');
 
-var emailValidTypo = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+const emailValidTypo = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+const numberTypo = /^[0-9]+$/; 
 
 
 //Launch verification on click
@@ -71,22 +83,23 @@ function functionValidation(e){
 
   birthValidation();
 
+  tournamentValidation();
+
+  cityValidate();
+
+  termsValidation();
+
 }
 
 
 //function validation first name
 function validerPrenom() {
-
   validerChampText('first', 'Prénom manquant')
-
 }
-
 
 //function validation last name
 function validerNom() {
-
   validerChampText('last', 'Nom manquant')
-
 }
 
 //function validation text last and first name
@@ -110,7 +123,7 @@ function validerChampText(fieldName, message) {
 function emailValidation() {
   var isValid = false;
   if (email.validity.valueMissing){
-    errorEmail.textContent = "Veuillez renseigner votre email";
+    errorEmail.textContent = "Renseignez votre email";
   }else if (emailValidTypo.test(email.value) == false){
     errorEmail.textContent = "Format email non valid";
   }else{
@@ -125,11 +138,59 @@ function emailValidation() {
 function birthValidation() {
   var isValid = false;
   if (birthdate.validity.valueMissing){
-    errorBirthdate.textContent = "Veuillez renseigner votre date de naissance";
+    errorBirthdate.textContent = "Renseignez votre date de naissance";
   }else{
     errorBirthdate.textContent = "";
     isValid = true;
   }
   return isValid;
 }
+
+
+//function validation number tournament
+function tournamentValidation() {
+  var isValid = false;
+  if (quantity.validity.valueMissing){
+    errorParticipation.textContent = "Indiquez un nombre de participation";
+  }else if (numberTypo.test(quantity.value) == false){
+    errorParticipation.textContent = "Utilisez uniquement des nombres";
+  }else{
+    errorParticipation.textContent = "";
+    isValid = true;
+  }
+  return isValid;
+}
+
+
+//function validation city
+function cityValidate() {
+  var isValid = false;
+  var auMoinsUneVilleSelectionnee = city1.checked || city2.checked || city3.checked || city4.checked || city5.checked || city6.checked;
+  if (quantity.value >= 1 && auMoinsUneVilleSelectionnee == false) {
+    errorCity.textContent = "Cochez au moins une case";
+  }else{
+    errorCity.textContent = "";
+    isValid = true;
+  }
+  return isValid;
+}
+
+
+//function validation terms od uses
+function termsValidation() {
+  var isValid = false;
+  if (checkTerms.checked == false){
+    console.log('no valid');
+    errorCondition.textContent = "Acceptez les conditions d'utilisation";
+  }else{
+    errorCondition.textContent = "";
+    isValid = true;
+    console.log('valid');
+  }
+  return isValid;
+}
+
+
+
+
 
