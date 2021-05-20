072041a6e5b8f489aeeb148dfff2c214f7749ba2
CROS <syl20cros@gmail.com>
2021-05-20 17:47:26 +0200

    feat (modal): add validation message

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -24,13 +24,13 @@ modalBtn.forEach((btn) => btn.addEventListener("click", displayMainModal));
 modalX.addEventListener('click', hideMainModal);
 
 // Open modal
-function displayMainModal(event) {
-  modalbg.classList.add('bground--show');
+function displayMainModal() {
+  modalbg.classList.add('bground-show');
 }
 
 // Close modal
-function hideMainModal(event) {
-  modalbg.classList.remove('bground--show');
+function hideMainModal() {
+  modalbg.classList.remove('bground-show');
 }
 
 
@@ -38,15 +38,15 @@ function hideMainModal(event) {
 /////////////////// form entries ///////////////////
 ////////////////////////////////////////////////////
 
+//////// variables and constants /////////
+const validation = document.getElementById('bt_valid');
 
-var validation = document.getElementById('bt_valid');
-
-var first = document.getElementById('first');
-var last = document.getElementById('last');
-var email = document.getElementById('email');
-var birthdate = document.getElementById('birthdate');
-var quantity = document.getElementById('quantity');
-var checkTerms = document.getElementById('checkbox1');
+const first = document.getElementById('first');
+const last = document.getElementById('last');
+const email = document.getElementById('email');
+const birthdate = document.getElementById('birthdate');
+const quantity = document.getElementById('quantity');
+const checkTerms = document.getElementById('checkbox1');
 const city1 = document.getElementById('location1');
 const city2 = document.getElementById('location2');
 const city3 = document.getElementById('location3');
@@ -54,17 +54,27 @@ const city4 = document.getElementById('location4');
 const city5 = document.getElementById('location5');
 const city6 = document.getElementById('location6');
 
-var first_missing = document.getElementById('first_missing');
-var last_missing = document.getElementById('last_missing');
-var errorEmail = document.getElementById('errorEmail');
-var errorBirthdate = document.getElementById('errorBirthdate');
-var errorParticipation = document.getElementById('errorParticipation');
-var errorCity = document.getElementById('errorCity');
-var errorCondition = document.getElementById('errorCondition');
+let first_missing = document.getElementById('first_missing');
+let last_missing = document.getElementById('last_missing');
+let errorEmail = document.getElementById('errorEmail');
+let errorBirthdate = document.getElementById('errorBirthdate');
+let errorParticipation = document.getElementById('errorParticipation');
+let errorCity = document.getElementById('errorCity');
+let errorCondition = document.getElementById('errorCondition');
 
 const emailValidTypo = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 const numberTypo = /^[0-9]+$/; 
 
+let firstIsValid;
+let lastIsValid;
+let emailIsValid;
+let birthdateIsValid;
+let numberTounementIsValid;
+let cityIsValid;
+let termsIsValid;
+
+
+let registration = document.getElementById('registrationOk');
 
 //Launch verification on click
 validation.addEventListener('click', functionValidation);
@@ -89,8 +99,15 @@ function functionValidation(e){
 
   termsValidation();
 
+  if(firstIsValid && emailIsValid && birthdateIsValid && numberTounementIsValid && cityIsValid && termsIsValid == true){
+    console.log('c est tout bon')
+    modalbg.classList.remove('bground-show');
+    registration.classList.add('registrationOk-show');
+  }else{
+    console.log('not good');
+    
+  }
 }
-
 
 //function validation first name
 function validerPrenom() {
@@ -104,93 +121,112 @@ function validerNom() {
 
 //function validation text last and first name
 function validerChampText(fieldName, message) {
-  var isValid = false;
+  firstIsValid = false;
   var domInput = document.getElementById(fieldName);
   var domMessage = document.getElementById(fieldName+'_missing');
   if (domInput.validity.valueMissing){
     domMessage.textContent = message;
+    domInput.classList.add('borderError');
   }else if (domInput.value.length <= 1){
     domMessage.textContent = "2 caractères minimum";
+    domInput.classList.add('borderError');
   }else{
     domMessage.textContent = "";
-    isValid = true;
+    firstIsValid = true;
+    domInput.classList.remove('borderError')
   }
-  return isValid;
+  return firstIsValid;
 }
 
 
 //function validation email
 function emailValidation() {
-  var isValid = false;
+  emailIsValid = false;
   if (email.validity.valueMissing){
     errorEmail.textContent = "Renseignez votre email";
+    email.classList.add('borderError');
   }else if (emailValidTypo.test(email.value) == false){
-    errorEmail.textContent = "Format email non valid";
+    errorEmail.textContent = "Format email non valide";
+    email.classList.add('borderError');
   }else{
     errorEmail.textContent = "";
-    isValid = true;
+    emailIsValid = true;
+    email.classList.remove('borderError');
   }
-  return isValid;
+  return emailIsValid;
 }
 
 
 //function validation birdh date
 function birthValidation() {
-  var isValid = false;
+  birthdateIsValid = false;
   if (birthdate.validity.valueMissing){
     errorBirthdate.textContent = "Renseignez votre date de naissance";
-  }else{
-    errorBirthdate.textContent = "";
-    isValid = true;
+    birthdate.classList.add('borderError');
+    return false;
   }
-  return isValid;
+  errorBirthdate.textContent = "";
+  birthdate.classList.remove('borderError');
+  birthdateIsValid = true;
+  return birthdateIsValid;
 }
 
 
 //function validation number tournament
 function tournamentValidation() {
-  var isValid = false;
+  numberTounementIsValid = false;
   if (quantity.validity.valueMissing){
     errorParticipation.textContent = "Indiquez un nombre de participation";
+    quantity.classList.add('borderError');
   }else if (numberTypo.test(quantity.value) == false){
     errorParticipation.textContent = "Utilisez uniquement des nombres";
+    quantity.classList.add('borderError');
   }else{
     errorParticipation.textContent = "";
-    isValid = true;
+    numberTounementIsValid = true;
+    quantity.classList.remove('borderError');
   }
-  return isValid;
+  return numberTounementIsValid;
 }
 
 
 //function validation city
 function cityValidate() {
-  var isValid = false;
+  cityIsValid = false;
   var auMoinsUneVilleSelectionnee = city1.checked || city2.checked || city3.checked || city4.checked || city5.checked || city6.checked;
   if (quantity.value >= 1 && auMoinsUneVilleSelectionnee == false) {
     errorCity.textContent = "Cochez au moins une case";
-  }else{
-    errorCity.textContent = "";
-    isValid = true;
+    return false; 
   }
-  return isValid;
+  errorCity.textContent = "";
+  cityIsValid = true;
+  return cityIsValid;
 }
 
 
 //function validation terms od uses
 function termsValidation() {
-  var isValid = false;
+  termsIsValid = false;
   if (checkTerms.checked == false){
-    console.log('no valid');
     errorCondition.textContent = "Acceptez les conditions d'utilisation";
-  }else{
-    errorCondition.textContent = "";
-    isValid = true;
-    console.log('valid');
+    return false; 
   }
-  return isValid;
+  errorCondition.textContent = "";
+  termsIsValid = true;
+  return termsIsValid;
 }
 
+////////////////////////////////////////////////////
+///////////// close WINDOW VALIDATION //////////////
+////////////////////////////////////////////////////
 
+const closeValidBtn = document.getElementById('btn-closeRegistrationOk');
 
+// Call function closeModal when X pressed
+closeValidBtn.addEventListener('click', hideWindowValidation);
 
-
+// Close window
+function hideWindowValidation() {
+  console.log('cliclic')
+  registration.classList.add('registrationOk-hide');
+}
